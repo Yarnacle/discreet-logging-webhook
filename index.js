62c767f3076bd8315caca3a06d57d1e905968979
Yarnacle <109094587+Yarnacle@users.noreply.github.com>
2025-07-22 20:03:48 -0500

    discord integration

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
-const fs = require('fs');
+import fs from 'fs';
+import moment from 'moment-timezone';
 
-const moment = require('moment-timezone');
+// Discord bot
+import { Client, Events, GatewayIntentBits,ChannelType } from 'discord.js';
 
+import config from './config.js';
+const { token,guildId,parentId } = config;
+
+const discClient = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+});
+
+discClient.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+discClient.login(token);
+
+const guild = await discClient.guilds.fetch(guildId);
+async function createChannel(name) {
+	const channel = await guild.channels.create({
+		name: name,
+		type: ChannelType.GuildText,
+		parent: parentId
+	});
+	console.log(`Channel created: ${channel.name}`);
+	return channel;
+}
+async function checkChannelExists(name) {
+	await guild.channels.fetch();
+
+	const existingChannel = guild.channels.cache.find(
+		(ch) => ch.name === name.toLowerCase() && ch.type === ChannelType.GuildText
+	);
+	return existingChannel;
+}
+
+// express webhook
 app.set('trust proxy',true)
 const port = 1095;
 app.listen(port,() => {
 	console.log(`Server listening on port ${port}`);
 });
 app.get('/',(req,res) => {
-	const client = req.headers['x-origin-client'];
+	const reqClient = req.headers['x-origin-client'];
 
 	const id = req.query.id;
 	const content = req.query.content;
+	const internal = req.query.internal;
 
 	if (id == undefined || content == undefined) {
 		console.log('Undefined field');
 		return res.status(404).send();
 	}
 
-	const type = client ? client:'Browser';
+	const type = reqClient ? reqClient:'Browser';
 
 	const ip = req.ip;
 	const timestamp = moment().tz('America/Chicago').format();
+	// log to file
 	fs.appendFile('server.log',`${type} [${timestamp}]-[${ip}]-[${id}]\n${content}\n`,error => {
 		if (error) {
 			throw error;
@@ -32,8 +68,17 @@ app.get('/',(req,res) => {
 		console.log(`Logged request from ${ip}`);
 	});
 
+	if (!internal) {
+		// log to discord
+		checkChannelExists(type).then(async channel => {
+			if (!channel) {
+				channel = await createChannel(type);
+			}
+			channel.send(`\`${id}\`\n\`\`\`${content}\`\`\``);
+		});
+	}
 
-	if (!client) {
+	if (!reqClient) {
 		return res.status(200).send('<center><h1>404 Not Found</h1></center>');
 	}
 	return res.status(200).send();
